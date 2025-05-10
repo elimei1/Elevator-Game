e62d72c066999a08d891b968212f8fd1f42c2b55
Lindrit14 <linrit_lindi@hotmail.com>
2025-05-10 16:05:38 +0200

    new scripts for progressbar

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -1,17 +1,54 @@
 extends CharacterBody2D
 class_name NPC
 
-@export var speed: float = 100.0
+@export var speed: float = 200.0
+@export var max_patience: float = 32.0    # maximale Breite der Bar in Pixeln
+@export var patience_loss_rate: float = 3 # Pixel pro Sekunde
+
+var patience: float
+var in_elevator: bool = false
+var can_move: bool = true
+
+@onready var anim = $AnimatedSprite2D
+@onready var bar  = $PatienceBar
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var in_elevator = false
+
+func _ready():
+	# Patience-Bar initialisieren
+	bar.min_value = 0
+	bar.max_value = max_patience
+	bar.step     = 3
+	patience     = max_patience
+	bar.value    = patience
+
+	# Starte mit Idle-Animation
+	anim.play("idle")
 
 func _physics_process(delta):
-	if not in_elevator:
-		velocity.x = -speed
+	# 1) Geduld konstant abbauen
+	patience = patience - patience_loss_rate * delta
+	if patience < 0:
+		patience = 0
+	bar.value = patience
 
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	else:
+	# 2) Wenn Geduld aufgebraucht → nach rechts zurücklaufen
+	if patience <= 0:
+		queue_free()
+		return
+
+	# 3) Wenn im Aufzug oder beim Warten → idle
+	if in_elevator or not can_move:
+		velocity.x = 0
 		velocity.y = 0
+		anim.play("idle")
+		return
 
+	# 4) Normal links laufen
+	velocity.x = -speed
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y = velocity.y + gravity * delta
+
+	anim.play("default")
 	move_and_slide()
--- scripts/npc_spawner.gd
+++ scripts/npc_spawner.gd
@@ -2,15 +2,25 @@ extends Node2D
 
 var npc_scene = preload("res://scenes/NPC.tscn")
 var floor_positions = [150, 350, 550]
-@export var number_of_npc = 3
+@export var number_of_npc =10
+
 
 func _on_timer_timeout() -> void:
+	if number_of_npc == 0:
+		return
+	
 	if number_of_npc <= 1:
 		get_node("Timer").stop()
+	
 	var npc = npc_scene.instantiate()
+	var npccChildNodes =npc.get_children()
+	
 	var floor_y = floor_positions.pick_random()
 	npc.position = Vector2(1100, floor_y)
 	add_child(npc)
-	number_of_npc -= 1
+	number_of_npc -= 1 
 	print(number_of_npc)
+	print(npccChildNodes)
+
+#func ready():	
 	
